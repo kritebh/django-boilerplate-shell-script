340095adec0a22eb970a2c456c12b5a34bc0af5f
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-06-22 16:37:46 +0530

    Virtual Environment added

--- django.ps1
+++ django.ps1
@@ -3,14 +3,14 @@ mkdir $folder
 cd $folder
 echo "Creating Virtual Environment"
 python -m venv env
-cd env/Scripts
-.\activate
-cd ../..
+echo "Activating Virtual Environment"
+.\env\Scripts\activate
 pip install django
 $project = Read-Host -Prompt "Project Name "
-django-admin startproject $project
+django-admin startproject $project .
 git init
 New-Item .gitignore -Value "\env"
+pip freeze > requirements.txt
 code .
 echo "Exiting...."
 Exit
--- djangoWithServer.ps1
+++ djangoWithServer.ps1
@@ -3,13 +3,11 @@ mkdir $folder
 cd $folder
 echo "Creating Virtual Environment"
 python -m venv env
-cd env/Scripts
-.\activate
-cd ../..
+echo "Activating Virtual Environment"
+.\env\Scripts\activate
 pip install django
 $project = Read-Host "Enter the project name "
-django-admin startproject $project
+django-admin startproject $project .
 git init
 New-Item .gitignore -Value "\env"
-cd $project
-python manage.py runserver
+python manage.py runserver 
