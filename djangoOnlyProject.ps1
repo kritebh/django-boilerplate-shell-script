d059b1ea647e38fa386cb6976c24979f7d0989c0
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-06-22 16:46:54 +0530

    Default spelling correction and removed deactivate

--- djangoOnlyProject.ps1
+++ djangoOnlyProject.ps1
@@ -1,23 +1,18 @@
-$folder = Read-Host "Enter the project name (Defualt: config)"
+$folder = Read-Host "Enter the project name (Default: config)"
 if(!$folder) {$folder = "config"}
 mkdir $folder
-
 cd $folder
 echo "Creating Virtual Environment"
 python -m venv env
+echo "Activating Virtual Environment"
 .\env\Scripts\activate
 pip install django
 django-admin startproject $folder .
 
 New-Item .gitignore -Value "env/"
 pip freeze > requirements.txt
-deactivate
 
 git init
-#git add .
-#git commit -m "Initial"
-
 code .
-
 echo "Exiting...."
 Exit
